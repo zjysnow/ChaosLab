0d593c9c818a93eaec6b8c743c5cf29e850be512
Albert Zou <zjysnow@outlook.com>
2021-10-27 22:39:02 +0800

    remove format for a while

--- Inception/ChaosCV/include/core/types.hpp
+++ Inception/ChaosCV/include/core/types.hpp
@@ -3,7 +3,7 @@
 #include "core/def.hpp"
 
 #include <ostream>
-#include <format>
+//#include <format>
 
 namespace chaos
 {
@@ -22,18 +22,18 @@ namespace chaos
 	static inline constexpr Complex operator ""i(long double im) { return Complex(0, static_cast<float>(im)); }
 	static inline constexpr Complex operator ""i(unsigned long long im) { return Complex(0, static_cast<float>(im)); }
 
-	static inline std::ostream& operator<<(std::ostream& stream, const Complex& complex)
-	{
-		return stream << std::format("{0:}{1:+}i", complex.re, complex.im);
-	}
-	template<class CharT>
-	struct std::formatter<chaos::Complex, CharT> : std::formatter<std::string, CharT>
-	{
-		template<class FormatContext>
-		auto format(const chaos::Complex& complex, FormatContext& fc)
-		{
-			std::string val = std::format("{}{:+}i", complex.re, complex.im);
-			return std::formatter<std::string, CharT>::format(val, fc);
-		}
-	};
+	//static inline std::ostream& operator<<(std::ostream& stream, const Complex& complex)
+	//{
+	//	return stream << std::format("{0:}{1:+}i", complex.re, complex.im);
+	//}
+	//template<class CharT>
+	//struct std::formatter<chaos::Complex, CharT> : std::formatter<std::string, CharT>
+	//{
+	//	template<class FormatContext>
+	//	auto format(const chaos::Complex& complex, FormatContext& fc)
+	//	{
+	//		std::string val = std::format("{}{:+}i", complex.re, complex.im);
+	//		return std::formatter<std::string, CharT>::format(val, fc);
+	//	}
+	//};
 }
